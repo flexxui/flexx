0c281576afefcc207b17b950f14814cf626e008f
Almar Klein <almar.klein@gmail.com>
2015-01-13 10:35:48 +0100

    init html5 runtime subpackage

---
+++ zoof/html5runtime/__init__.py
@@ -0,0 +1,18 @@
+"""
+This module provides a runtime to run applications based on HTML5 and
+associated technologies. There are several runtimes available:
+
+* xul - Mozilla's app framework. Make use of the same engine as Firefox.
+  Available where Firefox is installed.
+* nodewebkit - Node webkit is an app runtime based on Chromium and node.js.
+* pyqt - Use QWebkit as a runtime. No WebGL here though.
+* chrome-app
+* browser - launch a browser
+* browser-ff - launch firefox browser
+* browser-chrome - launch chrome browser
+* browser-ie - launch Internet Explorer
+
+"""
+
+def launch_html_runtime(url, runtime):
+    pass
