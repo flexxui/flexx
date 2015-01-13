f1eb699bc4270897a0a72a7243f8eb27661bbbf1
Almar Klein <almar.klein@gmail.com>
2015-01-13 10:33:20 +0100

    small changes

--- zoof/ui/xul.py
+++ zoof/ui/xul.py
@@ -2,31 +2,62 @@
 """
 
 import os
+import sys
 import time
 import shutil
 import subprocess
 import threading
 
+# todo: title should change with title of web page
+# todo: set position/size at runtime?
+# todo: enable fullscreen - does not seem to work on XUL
+# todo: test/fix on Windows, OSX, raspberry
 
-MAIN_JS = """
+
+APPLICATION_INI = """
+[App]
+Vendor=XULTest
+Name=myapp
+Version=1.0
+BuildID=20100902
+ID=xulapp@xultest.org
+
+[Gecko]
+MinVersion=1.8
+MaxVersion=200.*
 """
 
-MAIN_XUL = """<?xml version="1.0"?>
-
+MAIN_XUL = """
+<?xml version="1.0"?>
 <?xml-stylesheet href="chrome://global/skin/" type="text/css"?>
 
-<window xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
+<window 
+    xmlns="http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul"
+    id="webapp-window28"
     title="My Web App"
-    id="webapp-window"
+    windowtype="zoofui:main"
     width="800"
     height="600"
-    persist="screenX screenY width height sizemode">
+    sizemode="normal"
+    >
   <browser type="content-primary"
       src="{{SRC}}"
       flex="1"
       disablehistory="true"/>
+    <keyset>
+        <key id="key_fullScreen" keycode="VK_F11" command="View:FullScreen"/>
+    </keyset>
 </window>
+""".lstrip()
+
+
+MAIN_JS = """
 """
+
+
+# persist="screenX screenY width height sizemode"
+
+
 target = 'file:///home/almar/projects/pylib/zoof/zoof/exp/learn_html5.html'
 #target = "http://helloracer.com/webgl/"
 
@@ -37,26 +68,17 @@ MAIN_XUL = MAIN_XUL.replace('{{SRC}}', target)
 PREFS_JS = """
 pref("toolkit.defaultChromeURI", "chrome://myapp/content/main.xul");
 
+pref("toolkit.defaultChromeFeatures", "top=100, left=20000");
+ 
 /* debugging prefs, disable these before you deploy your application! */
-pref("browser.dom.window.dump.enabled", true);
-pref("javascript.options.showInConsole", true);
+pref("browser.dom.window.dump.enabled", false);
+pref("javascript.options.showInConsole", false);
 pref("javascript.options.strict", false);
-pref("nglayout.debug.disable_xul_cache", true);
-pref("nglayout.debug.disable_xul_fastload", true);
+pref("nglayout.debug.disable_xul_cache", false);
+pref("nglayout.debug.disable_xul_fastload", false);
 """
 
-APPLICATION_INI = """
-[App]
-Vendor=XULTest
-Name=myapp
-Version=1.0
-BuildID=20100901
-ID=xulapp@xultest.org
 
-[Gecko]
-MinVersion=1.8
-MaxVersion=200.*
-"""
 
 
 def create_xul_app(path):
@@ -67,7 +89,8 @@ def create_xul_app(path):
     
     # Create directory structure
     for subdir in ('',
-                   'chrome', 'chrome/content',
+                   'chrome', 'chrome/content', 
+                   'chrome/icons', 'chrome/icons/default',
                    'defaults', 'defaults/preferences',
                    ):
         os.mkdir(os.path.join(path, subdir))
@@ -81,13 +104,25 @@ def create_xul_app(path):
                         ('chrome/content/main.xul', MAIN_XUL),
                         ]:
         open(os.path.join(path, fname), 'wb').write(text.encode('utf-8'))
+    
+    # Icon
+    shutil.copy('/home/almar/projects/pyapps/iep/default/iep/resources/appicons/ieplogo64.png',
+                os.path.join(path, 'chrome/icons/default/webapp-window28.png'))
+    
+    # Create link, so that our window is not grouped with ff, and has
+    # a more meaningful process name.
+    # todo: if frozen, call it myapp-ui, otherwise, python-zoof-ui
+    os.symlink('/usr/lib/firefox/firefox', os.path.join(path, 'python-zoof-ui'))
 
 
 def launch():
     
     create_xul_app('/home/almar/projects/pylib/zoof/zoof/exp/xulapp2')
     
-    cmd = ['firefox', '-app', '/home/almar/projects/pylib/zoof/zoof/exp/xulapp2/application.ini']
+    
+    cmd = ['/home/almar/projects/pylib/zoof/zoof/exp/xulapp2/python-zoof-ui', 
+           '-app', 
+           '/home/almar/projects/pylib/zoof/zoof/exp/xulapp2/application.ini']
     
     env = os.environ.copy()
     p = subprocess.Popen(cmd, env=env,
