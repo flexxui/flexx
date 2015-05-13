38be5bab69f4aeac9366a239e9b6ca2e752f0ec1
Almar Klein <almar.klein@gmail.com>
2015-05-13 16:40:21 +0200

    refactor widgets

--- flexx/ui/__init__.py
+++ flexx/ui/__init__.py
@@ -5,9 +5,7 @@ GUI toolkit based on web technology with a Pythonic API.
 
 from .app import run, stop, App, call_later
 from .app import Mirrored, get_instance_by_id
-#from .widget import Widget, Window, Label, Button
-#from .widget import HBox, VBox, Form, Grid, PinBoard
-#from .widget import HSplit
-#from .widget import PHSplit, PDockArea, MenuItem, Menu, MenuBar
 
-from . import widget2
+from .widget import Widget  #, Window
+from .button import Label, Button
+from .layout import HBox, VBox  #, Form, Grid, PinBoard
--- flexx/ui/widget.py
+++ flexx/ui/widget.py
@@ -1,320 +1,152 @@
+""" The base widget class.
+"""
 
 import json
 
-from .app import App, get_default_app
+from ..pyscript import js
+from ..properties import Prop, Instance, Str, Tuple, Float
+from . import Mirrored, get_instance_by_id
 
-
-class NativeElement(object):
+class WidgetProp(Prop):
+    _default = None
     
-    def __init__(self, id):
-        t = 'document.getElementById("{id}").innerHTML = "{text}"'
-        # ... get __dir__ from JS and allow live inspection of DOM elements
-        # That would be great for debugging ...
-
-
-class Widget(object):
-    """ Base widget class
+    def validate(self, val):
+        if val is None or isinstance(val, Widget):
+            return val
+        else:
+            raise ValueError('Prop %r must be a Widget or None, got %r' % 
+                             (self.name, val.__class__.__name__))
     
-    All widgets derive from this class. On itself, this type of widget
-    represents an empty space, and can be useful as a filler.
-    
-    Properties
-    ----------
-    parent (widget) : the widget that this widget is a child of.
-    flex (scalar|tuple) : how flexible this widget is when used in a
-        layout such as an hbox or vbox. By using a tuple, the flex can
-        be set independently for the horizontal and vertical direction.
-    pos (tuple) : the position of this widget in a Grid or PinBoard layout.
-    min_width (scalar) : the minimum width of the widget in pixels.
-    min_height (scalar) : the minimum height of the widget in pixels.
-    css (str) : css styling to apply to the HTML element that represents
-         this widget, e.g. ``css='background: #f00; padding: 2em;'``.
-    """
-    
-    _counter = 0  # to produce unique id's
-    
-    def __init__(self, parent=None, flex=0, pos=(0, 0), 
-                 min_width=0, min_height=0, css=''):
-        if parent is None:
-            if _default_parent:
-                parent = _default_parent[-1]
-            else:
-                parent = get_default_app()
-                #raise ValueError('Parent must be given unless it is '
-                #                 'instantiated a widget context.')
-        
-        self._parent = None
-        self._children = []
-        self._set_parent(parent)
-        
-        self._flex = flex if isinstance(flex, tuple) else (flex, flex)
-        self._pos = pos
-        app = self.get_app()
-        app._widget_counter += 1
-        self._id = self.__class__.__name__ + str(app._widget_counter)
-        
-        self._min_width = min_width
-        self._min_height = min_height
-        self._css = css
-        
-        # Call function to create js_object
-        self._create_js_object()
-    
-    def __enter__(self):
-        _default_parent.append(self)
-        return self
-    
-    def __exit__(self, type, value, traceback):
-        assert self is _default_parent.pop(-1)
+    def to_json(self, value):
+        # Widgets are kept track of via their id
         if value is None:
-            self.update()
+            return json.dumps(None)
+        return json.dumps(value.id)
     
-    @property
-    def parent(self):
-        return self._parent
+    def from_json(self, txt):
+        return get_instance_by_id(json.loads(txt))
     
-    def _set_parent(self, new_parent):
-        old_parent = self._parent
-        if old_parent is not None:
-            while self in old_parent._children:
-                old_parent._children.remove(self)
-        if new_parent is not None:
-            new_parent._children.append(self)
-        self._parent = new_parent
+    @js
+    def to_json__js(self, value):
+        if value is None:
+            return JSON.stringify(None)
+        else:
+            return JSON.stringify(value.id)
     
-    @property
-    def children(self):
-        return list(self._children)
+    @js
+    def from_json__js(self, value):
+        return flexx.instances[JSON.parse(value)]
+
+
+# todo: would be nice if Tuple just worked with elements that need custom json ...
+class WidgetsProp(Tuple):
     
-    def update(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        Tuple.__init__(self, WidgetProp, *args, **kwargs)
     
-    def _create_js_object(self, **kwargs):
-        """ This method can be overloaded to populate the dict used
-        by JS to create the widget. Overloaded versions should simpy
-        call the super-method with additional kwargs.
-        """
+    @js
+    def to_json__js(self, value):
+        res = '  '
+        for v in value:
+            if v is None or v.id is undefined:
+                res += JSON.stringify(None)
+            else:
+                res += JSON.stringify(v.id)
+            res += ', '
+        return '[' + res[:-2] + ']'
+    
+    @js
+    def from_json__js(self, value):
+        res = []
+        for v in JSON.parse(value):
+            res.append(flexx.instances[v])
+        return res
+
+
+class Widget(Mirrored):
+    """ Base widget class.
+    """
+    
+    CSS = """
+    
+    """
+    
+    container_id = Str()  # used if parent is None
+    
+    parent = WidgetProp()
+    #children = Tuple(WidgetProp)  # todo: can we make this readonly?
+    children = WidgetsProp()
+    
+    flex = Float()
+    min_width = Float()
+    min_height = Float()  # todo: or min_size?
+    cssClassName = Str()  # todo: should this be private? Or can we calculate it in JS?
+    
+    
+    def __init__(self, parent=None, **kwargs):
+        # todo: -> parent is widget or ref to div element
         
-        # Get css classes
+        # Provide css class name to 
         classes = ['zf-' + c.__name__.lower() for c in self.__class__.mro()]
-        classes = ' '.join(classes[:1-len(Widget.mro())])
+        classname = ' '.join(classes[:1-len(Widget.mro())])
         
-        # Get parent
-        parent = 'body' if isinstance(self.parent, App) else self.parent.id
+        # Pass properties via kwargs
+        kwargs['cssClassName'] = classname
+        kwargs['parent'] = parent
         
-        self._create_js_object_real(id=self.id,
-                                    className=classes,
-                                    
-                                    parent=parent,
-                                    pos=self._pos,
-                                    hflex=self._flex[0],
-                                    vflex=self._flex[1],
-                                    css=self._css,
-                                    min_width=self._min_width,
-                                    min_height=self._min_height,
-                                    **kwargs)
-    
-    def _create_js_object_real(self, **kwargs):
+        Mirrored.__init__(self, **kwargs)
         
-        eval = self.get_app()._exec
-        funcname = 'create' + self.__class__.__name__
-        eval('flexx.%s(%s);' % (funcname, json.dumps(kwargs)))
-    
-    def get_app(self):
-        node = self.parent
-        while not isinstance(node, App):
-            node = node.parent
-        return node
-    
-    @property
-    def id(self):
-        return self._id
-    
-
-
-class Window(object):
-    
-    __slots__ = ['_title', '_parent']
-    
-    _TEMPLATE = """
-        window.win{id} = window.open('{url}', '_blank', '{specs}');
-        """
-    
-    def __init__(self, parent, title='new window'):
-        assert isinstance(parent, App)
-        self._parent = parent
-        self._title = title
-        if parent._ws:
-            self._create()
-    
-    def _create(self):
-        t = self._TEMPLATE.format(id=id(self), url='about:blank', specs='')
-        print(t)
-        # arg, this needs to go in an onload
-        t += '\nwindow.win{id}.document.body.innerHTML = "";'.format(id=id(self))
-        self._parent._exec(t)
-    
-    def close(self):
-        self._parent._exec('window.win{id}.close()'.format(id=id(self)))
-        
-    def set_title(self, title):
-        pass
-        # todo: properties or functions?
-
-
-class Label(Widget):
-    """ A Label represents a piece of text.
-    """
-    
-    def __init__(self, parent=None, text='', **kwargs):
-        self._text = text
-        super().__init__(parent, **kwargs)
-    
-    def _create_js_object_real(self, **kwargs):
-        super()._create_js_object_real(text=self._text, **kwargs)
-        
-    def set_text(self, text):
-        self._text = text
-        t = 'document.getElementById("{id}").innerHTML = "{text}"'
-        self.get_app()._exec(t.format(id=self._id, text=text))
-
-
-## Try Py -> JS decorator
-
-from .compile import js
-
-
-class Button(Widget):
-    """ A Button is a widget than can be clicked on to invoke an action
-    """
-    
-    def __init__(self, parent=None, text='Click me', **kwargs):
-        self._text = text
-        super().__init__(parent, **kwargs)
-        
-        eval = self.get_app()._exec
-        # todo: arg .js -> I want eady access to info about that func
-        eval('flexx.instances.%s.%s' % (self.id, self.hello.js.jscode[5:]))
-        eval('flexx.instances.%s.%s' % (self.id, self.set_text.js.jscode[5:]))
-    
-    def _create_js_object_real(self, **kwargs):
-        super()._create_js_object_real(text=self._text, **kwargs)
-    
-    def _set_text(self, text):
-        self._text = text
-        t = 'document.getElementById("{id}").innerHTML = "{text}"'
-        self.get_app()._exec(t.format(id=self._id, text=text))
+        #self._js_init(classes)  # todo: allow a js __init__
     
     @js
-    def set_text(self, text):
-        this.innerHTML = text  # todo: self -> this
+    def _js_cssClassName_changed(self, name, old, className):
+        if this.node:
+            this.node.className = className
     
     @js
-    def hello(self, text):
-        print('Hello world!')
-
-_default_parent = []
-
-
-class Layout(Widget):
-    """ Base class for all layouts
-    """
+    def _js_set_child(self, widget):
+        # May be overloaded in layout widgets
+        self.node.appendChild(widget.node)
     
-    def __init__(self, parent=None, spacing=None, margin=None, **kwargs):
-        self._spacing = spacing
-        self._margin = margin
-        super().__init__(parent, **kwargs)
+    def _parent_changed(self, name, old_parent, new_parent):
+        if old_parent is not None:
+            children = list(old_parent.children)
+            while self in children:
+                children.remove(self)
+            old_parent._set_prop('children', children)  # bypass readonly
+        if new_parent is not None:
+            children = list(new_parent.children)
+            children.append(self)
+            new_parent._set_prop('children', children)
     
-    def _create_js_object_real(self, **kwargs):
-        spacing = str(self._spacing or 0) + 'px'
-        margin = str(self._margin or 0) + 'px'
-        super()._create_js_object_real(spacing=spacing, margin=margin, **kwargs)
-
-    def update(self):
-        eval = self.get_app()._exec
-        eval('flexx.get(%r).applyLayout();' % self.id)
+    @js
+    def _parent_changed__js(self, name, old_parent, new_parent):
+        if old_parent is not None:
+            children = old_parent.children
+            while children.indexOf(self) >= 0:  # todo: "self in children"
+                children.remove(self)
+            #old_parent._set_prop('children', children)  # bypass readonly
+            old_parent.children = children
+        if new_parent is not None:
+            children = new_parent.children
+            children.append(self)
+            #new_parent._set_prop('children', children)
+            new_parent.children = children
+            new_parent._set_child(self)
     
-
-# todo: spacing and margin for all layouts
-# todo: combine JS code for layouts?
-
-class HBox(Layout):
-    """ An HBox is a layout widget used to align widgets horizontally
-    """
-
-
-class VBox(Layout):
-    """ An VBox is a layout widget used to align widgets vertically
-    """
-
-
-class Form(Layout):
-    """ A Form layout vertically stacks pairs of widgets
+    @js
+    def set_cointainer_id(self, id):
+        #if self._parent:
+        #    return
+        print('setting container id', id)
+        el = document.getElementById(id)
+        el.appendChild(this.node)
     
-    Usually, these will be pairs of labels vs input widgets. The labels
-    are on the left with flex 0, and the widgets on the right have flex
-    1. The same can be achieved with a Grid, but a Form provides a more
-    convienent API for this use-case.
-    """
-
-
-class Grid(Layout):
-    """ A Grid layout aligns widgets in a grid
-    
-    It is more flexible than hbox and vbox, but is also slighly more
-    complex to use.
-    """
-    
-    # todo: allow different flexes per row/col
-    # e.g. set_col_flex(i, flex), set_row_flex(i, flex)
-    # todo: allow colspan and rowspan
-
-
-class PinBoard(Layout):
-    """ A PinBoard layout allows absolute positioning of its contained widgets
-    
-    The ``pos`` property of child widgets should be set. When larger
-    than 1, the value is interpreted as pixels. If smaller than 1, it
-    is interpreted as a percentage of the layout's size.
-    """
-
-
-class HSplit(Layout):
-    """ The HSplit horizontally splits the available space in regions,
-    which size can be set by the user by dragging the divider.
-    """
-
-
-class PHSplit(Layout):
-    """ Phosphor splitter
-    """
-
-
-class PDockArea(Layout):
-    """ Phosphor dock area
-    """
-
-class MenuBar(Widget):
-    """ Phosphor menu bar
-    """
-
-class Menu(Widget):
-    """ Phosphor menu
-    """
-    def __init__(self, parent=None, text='Blaa', **kwargs):
-        self._text = text
-        super().__init__(parent, **kwargs)
-    
-    def _create_js_object_real(self, **kwargs):
-        super()._create_js_object_real(text=self._text, **kwargs)
-        
-
-class MenuItem(Widget):
-    """ Phosphor menu item
-    """
-    def __init__(self, parent=None, text='action', **kwargs):
-        self._text = text
-        super().__init__(parent, **kwargs)
-    
-    def _create_js_object_real(self, **kwargs):
-        super()._create_js_object_real(text=self._text, **kwargs)
+    def _repr_html_(self):
+        container_id = self.id + '_container'
+        # Set container id, this gets applied in the next event loop
+        # iteration, so by the time it gets called in JS, the div that
+        # we define below will have been created.
+        from .app import call_later
+        call_later(0, self.set_cointainer_id, container_id) # todo: always do calls in next iter
+        return "<div id=%s />" % container_id
