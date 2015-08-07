e99e32302f348353a5b21aab802198abd67e5159
Almar Klein <almar.klein@gmail.com>
2015-08-08 00:57:38 +0200

    react: functional tests 100%

--- flexx/react/tests/test_functional.py
+++ flexx/react/tests/test_functional.py
@@ -49,4 +49,67 @@ def test_filter():
     assert registered == [3, 3, 4]
 
 
+def test_reduce1():
+    
+    registered = []
+    
+    @react.input
+    def number(n=0):
+        return float(n)
+    
+    @react.connect(react.reduce(lambda x, y: x+y, 'number'))
+    def reg1(v):
+        registered.append(v)
+    
+    number(1)
+    number(2)
+    number(2)
+    number(3)
+    
+    assert registered == [0, 1, 3, 5, 8]
+
+
+def test_reduce2():
+    
+    registered = []
+    
+    @react.input
+    def number(n=0):
+        return float(n)
+    
+    @react.connect(react.reduce(lambda x, y: x+y, 'number', 1))
+    def reg1(v):
+        registered.append(v)
+    
+    number(1)
+    number(2)
+    number(2)
+    number(3)
+    
+    assert registered == [1, 2, 4, 6, 9]
+
+
+def test_merge():
+    
+    registered = []
+    
+    @react.input
+    def number1(n=0):
+        return float(n)
+    
+    @react.input
+    def number2(n=0):
+        return float(n)
+    
+    @react.connect(react.merge('number1', 'number2'))
+    def reg1(v):
+        registered.append(v)
+    
+    number1(1)
+    number2(2)
+    number2(3)
+    
+    assert registered == [(0, 0), (1, 0), (1, 2), (1, 3)]
+
+
 run_tests_if_main()
