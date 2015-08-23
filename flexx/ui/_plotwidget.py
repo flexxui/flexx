5efaf6a1f0942de662e0e4e61f0ddf04a5e8332f
Almar Klein <almar.klein@gmail.com>
2015-08-23 16:34:11 +0200

    ui: plotwidget tweak example

--- flexx/ui/_plotwidget.py
+++ flexx/ui/_plotwidget.py
@@ -23,31 +23,28 @@ Interactive example:
     
     class Example(ui.Widget):
         def init(self):
-            time = [i/10 for i in range(100)]
+            time = [i/100 for i in range(100)]
             with ui.VBox():
                 with ui.HBox():
-                    ui.Label(text='Amplitude:')
+                    ui.Label(text='Frequency:')
                     self.slider1 = ui.Slider(min=1, max=10, value=5, flex=1)
                     ui.Label(text='Phase:')
                     self.slider2 = ui.Slider(min=0, max=6, value=0, flex=1)
                 self.plot = ui.PlotWidget(flex=1, xdata=time, xlabel='time',
-                                          ylabel='amp', title='A sinusoid')
+                                            ylabel='amplitude', title='a sinusoid')
         
         class JS:
             
             @react.connect('slider1.value', 'slider2.value')
-            def __update_amplitude(self, amp, phase):
+            def __update_amplitude(self, freq, phase):
                 ydata = []
                 for x in self.plot.xdata():
-                    ydata.append(amp*Math.sin(x+phase))
+                    ydata.append(Math.sin(freq*x*2*Math.PI+phase))
                 self.plot.ydata(ydata)
 """
 
-
-# from .. import react
-# from . import Widget
-from flexx import react
-from flexx.ui import Widget
+from .. import react
+from . import Widget
 
 
 class PlotWidget(Widget):
