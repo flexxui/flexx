be3916a346818480b7ba84311d9189d513b72a60
Almar Klein <almar.klein@gmail.com>
2016-01-21 23:39:54 +0100

    does the cli test pass now on travis?

--- flexx/util/tests/test_cli.py
+++ flexx/util/tests/test_cli.py
@@ -7,7 +7,7 @@ from flexx.util.testing import run_tests_if_main, raises
 
 def test_cli():
     cmd = [sys.executable, '-m', 'flexx', 'version']
-    v = subprocess.check_output(cmd, shell=True).decode().strip()
+    v = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode().strip()
     assert v == flexx.__version__
 
     
