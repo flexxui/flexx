7275d4b8bfad1f235a5c489f809d49d1e418d308
Almar Klein <almar.klein@gmail.com>
2016-05-18 23:24:34 +0200

    flake

--- flexx/_config.py
+++ flexx/_config.py
@@ -3,5 +3,5 @@ from .util.config import Config
 config = Config('flexx', '~appdata/.flexx.cfg',
     hostname=('localhost', str, 'The default port to serve apps.'),
     port=(0, int, 'The default port to serve apps. Zero means auto-select.'),
-    webruntime=('', str, 'The default web runtime to use. Default is xul/browser.')
+    webruntime=('', str, 'The default web runtime to use. Default is xul/browser.'),
     )
