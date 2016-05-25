44020b25a906612f11d873e1f0f51880261dda42
Almar Klein <almar.klein@gmail.com>
2016-05-25 16:50:43 +0200

    Add loging func to Flexx

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,8 @@ sys.path.insert(0, os.path.abspath('scripts'))
 import flexx
 from flexx import util, webruntime
 
+flexx.set_log_level('warning')  # Don't spam the Sphynx output
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
--- flexx/_config.py
+++ flexx/_config.py
@@ -1,6 +1,7 @@
 from .util.config import Config
 
 config = Config('flexx', '~appdata/.flexx.cfg',
+    log_level=('info', str, 'The log level to use (DEBUG, INFO, WARNING, ERROR)'),
     hostname=('localhost', str, 'The default port to serve apps.'),
     port=(0, int, 'The default port to serve apps. Zero means auto-select.'),
     webruntime=('', str, 'The default web runtime to use. Default is xul/browser.'),
