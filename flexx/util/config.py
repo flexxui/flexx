2db5eb08cd58b9ccd4a5cc4bbd905366933a0af8
Samuel Loury <konubinixweb@gmail.com>
2016-05-27 22:07:03 +0200

    Use the xdg standard to store the config in linux

--- flexx/util/config.py
+++ flexx/util/config.py
@@ -8,7 +8,7 @@ import logging
 if sys.version_info[0] == 2:  # pragma: no cover
     from ConfigParser import ConfigParser as _ConfigParser
     from StringIO import StringIO
-    
+
     class ConfigParser(_ConfigParser):
         def read_string(self, string, source):
             return self.readfp(StringIO(string), source)
@@ -53,37 +53,37 @@ else:
     TYPEMAP[str] = str
 
 INSTANCE_DOCS = """ Configuration object for {name}
-    
+
     The options below can be set from different sources, and are
     evaluated in the following order:
-    
+
     * From the default value.
     * From .cfg or .ini file, or a string in cfg format.
     * From environment variables, e.g. ``{NAME}_FOO=3``.
     * From command-line arguments, e.g. ``--{name}-foo=3``.
     * From setting the config option directly, e.g. ``config.foo = 3``.
-    
+
     Use ``print(config)`` to get a summary of the current values and
     from which sources they were set.
-    
+
     Parameters:
     """
 
 
 class Config(object):
     """ Class for configuration objects.
-    
+
     A Config object has a set of options, which can be str, int, float,
     bool, or a tuple of any of the above. Options can be set from
     different sources:
-    
+
     * Each option has a default value.
     * From .cfg or .ini files.
     * From strings in ini format.
     * From environment variables.
     * From command-line arguments.
     * By setting the config option directly.
-    
+
     Parameters:
         name (str): the name by which to identify this config. This name
             is used as a prefix in environment variables and command
@@ -95,56 +95,56 @@ class Config(object):
             home dir and appdata dir.
         **options: The options specification: each option consists of
             a 3-element tuple (default, type, docstring).
-    
+
     Example:
-    
+
         .. code-block:: Python
-        
+
             config = Config('myconfig', '~appdata/.myconfig.cfg',
                             foo=(False, bool, 'Whether to foo'),
                             bar=(0.0, float, 'The size of the bar'),
                             spam=('1,2,3', [int], 'A tuple of ints'))
-        
+
         With this, options can be set:
-    
+
         * With an entry ``foo = 3`` in '~appdata/.myconfig.cfg'.
         * With a string ``"foo = 3"`` passed at initialization.
-        * With an environment variable named ``MYCONFIG_FOO``. 
+        * With an environment variable named ``MYCONFIG_FOO``.
         * With a command line argument ``--myconfig-foo=3``.
         * By doing ``config.foo = 3``, or ``config['foo'] = 3`` in Python.
-    
+
     Notes:
         * Option names are case insensitive, except for attribute access and
           environment variables (the latter must be all uppercase).
         * All values can be set as a Python object or a string; they
           are automatically converted to the correct type.
-        * Each instance gets a docstring that lists all options, so it 
+        * Each instance gets a docstring that lists all options, so it
           can easily be used in e.g. Sphynx docs.
     """
-    
+
     def __init__(self, name, *sources, **options):
-        
+
         # The identifier name for this config
         self._name = name
         if not is_valid_name(name):
             raise ValueError('Config name must be an alphanumeric string, '
                              'starting with a letter.')
-        
+
         # The option names (unmodified case)
         self._options = []
-        
+
         # Where the values are stored, we keep a stack, lowercase keys
         self._opt_values = {}  # name -> list of (source, value) tuples
-        
+
         # Map of lowercase option names to validator functions
         self._opt_validators = {}
-        
+
         # Map of lowercase option names to type names, for better reporting
         self._opt_typenames = {}
-        
+
         # Map of lowercase option names to docstrings
         self._opt_docs = {}
-        
+
         # Parse options
         option_docs = ['']
         for name in sorted(options.keys(), key=lambda x: x.lower()):
@@ -174,18 +174,18 @@ class Config(object):
                                            if istuple else TYPEMAP[typ])
             self._opt_docs[lname] = doc
             self._opt_values[lname] = []
-        
+
         # Overwrite docstring
         self.__doc__ = INSTANCE_DOCS.format(name=self._name,
                                             NAME=self._name.upper())
         self.__doc__ += '\n'.join(option_docs)
-        
+
         # --- init values
-        
+
         # Set defaults
         for name, spec in options.items():
             self._set('default', name, spec[0])
-        
+
         # Load from sources
         for source in sources:
             if not isinstance(source, basestring):
@@ -194,14 +194,14 @@ class Config(object):
                 self.load_from_string(source)
             else:
                 self.load_from_file(source)
-        
+
         # Load from environ
         for name in self._opt_values:
             env_name = (self._name + '_' + name).upper()
             value = os.getenv(env_name, None)  # getenv is case insensitive
             if value is not None:
                 self._set('environ', name, value)
-        
+
         # Load from argv
         arg_prefix = '--' + self._name.lower() + '-'
         for i in range(1, len(sys.argv)):
@@ -210,11 +210,11 @@ class Config(object):
                 name, value = arg[len(arg_prefix):].split('=', 1)
                 if name.lower() in self._opt_values:
                     self._set('argv', name, value)
-    
+
     def __repr__(self):
         t = '<Config %r with %i options at 0x%x>'
         return t % (self._name, len(self._options), id(self))
-    
+
     def __str__(self):
         # Return a string representing a summary of the options and
         # how they were set from different sources.
@@ -230,22 +230,22 @@ class Config(object):
                 lines.append('    %r from %s' % (val, source))
             lines[-1] = ' -> ' + lines[-1][4:]  # Mark current value
         return '\n'.join(lines)
-    
+
     def __len__(self):
         return len(self._options)
-    
+
     def __iter__(self):
         return self._options.__iter__()
-    
+
     def __dir__(self):
         return self._options
-    
+
     def __getattr__(self, name):
         # Case sensitive get
         if not name.startswith('_') and name in self._options:
             return self._opt_values[name.lower()][-1][1]
         return super(Config, self).__getattribute__(name)
-    
+
     def __getitem__(self, name):
         # Case insensitive get
         if not isinstance(name, basestring):
@@ -254,13 +254,13 @@ class Config(object):
             return self._opt_values[name.lower()][-1][1]
         else:
             raise IndexError('Config has no option %r' % name)
-    
+
     def __setattr__(self, name, value):
         # Case sensitive set
         if not name.startswith('_') and name in self._options:
             return self._set('set', name, value)
         return super(Config, self).__setattr__(name, value)
-    
+
     def __setitem__(self, name, value):
         # Case insensitve set
         if not isinstance(name, basestring):
@@ -269,7 +269,7 @@ class Config(object):
             return self._set('set', name, value)
         else:
             raise IndexError('Config has no option %r' % name)
-    
+
     def _set(self, source, name, value):
         # The actual setter (case insensitive), applies the validator
         validator = self._opt_validators[name.lower()]
@@ -284,7 +284,7 @@ class Config(object):
         else:
             stack.append((source, real_value))
             stack.sort(key=stack_sorter)
-    
+
     def load_from_file(self, filename):
         """ Load config options from a file, as if it was given as a
         source during initialization. This means that options set via
@@ -304,7 +304,7 @@ class Config(object):
                              (filename, str(err)))
                 return
             self.load_from_string(text, filename)
-    
+
     def load_from_string(self, text, filename='<string>'):
         """ Load config options from a string, as if it was given as a
         source during initialization. This means that options set via
@@ -314,7 +314,7 @@ class Config(object):
             self._load_from_string(text, filename)
         except Exception as err:
             logging.warn(str(err))
-    
+
     def _load_from_string(self, s, filename):
         # Create default section, so that users can work with sectionless
         # files (as is common in an .ini file)
@@ -339,7 +339,7 @@ def isidentifier(s):
     # http://stackoverflow.com/questions/2544972/
     if not isinstance(s, basestring):  # noqa
         return False
-    return re.match(r'^\w+$', s, re.UNICODE) and re.match(r'^[0-9]', s) is None 
+    return re.match(r'^\w+$', s, re.UNICODE) and re.match(r'^[0-9]', s) is None
 
 
 # From pyzolib/paths.py (https://bitbucket.org/pyzo/pyzolib/src/tip/paths.py)
@@ -358,7 +358,9 @@ def appdata_dir(appname=None, roaming=False):
         path = os.path.join(userDir, 'Library', 'Application Support')
     # On Linux and as fallback
     if not (path and os.path.isdir(path)):  # pragma: no cover
-        path = userDir
+        path = os.environ.get(
+            "XDG_CONFIG_HOME",
+            os.path.expanduser(os.path.join("~", ".config")))
     # Maybe we should store things local to the executable (in case of a
     # portable distro or a frozen application that wants to be portable)
     prefix = sys.prefix
@@ -387,7 +389,7 @@ def appdata_dir(appname=None, roaming=False):
 
 
 if __name__ == '__main__':
-    
+
     sys.argv.append('--test-foo=8')
     c = Config('test',
                foo=(3, int, 'foo yeah'),
