14718e46ae5796771fe7a0bb70b6a2b4f8c97b17
Almar Klein <almar.klein@gmail.com>
2016-07-01 15:39:53 +0200

    bah, forgot import

--- docs/scripts/uiexample.py
+++ docs/scripts/uiexample.py
@@ -55,6 +55,7 @@ def create_ui_example(filename, to_root, height=300):
             m = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(m)
         else:  # http://stackoverflow.com/a/67692/2271927
+            from importlib.machinery import SourceFileLoader
             SourceFileLoader("example", filename).load_module()
     except Exception as err:
         err_text = str(err)
