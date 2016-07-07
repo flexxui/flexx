5bf3b253e70efa8df1720dc8d8d3c71762597562
Almar Klein <almar.klein@gmail.com>
2016-07-07 12:03:42 +0200

    Tweaks to colab painting example

--- flexx/ui/examples/colab_painting.py
+++ flexx/ui/examples/colab_painting.py
@@ -1,11 +1,8 @@
 """
 A web app that allows multiple people to colaborate in painting. People
-connecting later will not see the paint that was added earlier. The
-paint slowly fades (like memory) as more paint is added, so the
-appearance will be more similar as people are connected longer (people
-"align" as they work together). Each person is assigned a random color
-(his/her "skill") which might affect how that person can best contribute
-to the painting.
+connecting later will not see the "paint" that was added earlier. Each
+person is assigned a random color which affects how that person can
+best contribute to the painting.
 """
 
 import random
@@ -22,8 +19,12 @@ class Relay(event.HasEvents):
     """ Global object to relay paint events to all participants.
     """
     @event.emitter
-    def new_dot(self, pos, color):
+    def global_paint(self, pos, color):
         return dict(pos=pos, color=color)
+
+
+# Create global relay
+relay = Relay()
 
 
 class ColabPainting(ui.Widget):
@@ -43,8 +44,6 @@ class ColabPainting(ui.Widget):
     def init(self):
         
         # Select random color
-        #rgb = [str(random.randint(0, 200)) for i in range(3)]
-        #self.color = 'rgb(%s)' % (', '.join(rgb))
         self.color = random.choice(COLORS)
         
         # App layout
@@ -57,20 +56,33 @@ class ColabPainting(ui.Widget):
                 ui.Widget(flex=1)
             ui.Widget(flex=1)
         
-        # Pipe messages send by the relay into this app
-        relay.connect(self._push_info, 'new_dot:' + self.id)
+        # Connect events
+        self.connect(self._this_user_adds_paint, 'canvas.mouse_down')
+        relay.connect(self._any_user_adds_paint, 'global_paint:' + self.id)
         
         # Start people-count-updater
         self._update_participants()
     
-    def _push_info(self, *events):
+    @event.prop
+    def color(self, color='#000'):
+        """ The selected color for the current session. """
+        return str(color)
+    
+    def _this_user_adds_paint(self, *events):
+        """ Detect mouse down, emit global paint event via the relay. """
+        for ev in events:
+            relay.global_paint(ev.pos, self.color)
+    
+    def _any_user_adds_paint(self, *events):
+        """ Receive global paint event from the relay, emit local paint event. """
         if self.session.status:
             for ev in events:
-                self.emit('new_dot', ev)
+                self.emit('paint', ev)
     
     def _update_participants(self):
+        """ Keep track of the number of participants. """
         if not self.session.status:
-            relay.disconnect('new_dot:' + self.id)
+            relay.disconnect('new_dot:' + self.id)  # clean up
             return  # and dont't invoke a new call
         proxies = app.manager.get_connections(self.__class__.__name__)
         n = len(proxies)
@@ -78,16 +90,6 @@ class ColabPainting(ui.Widget):
         self.people.text = '%i persons are painting' % n
         app.call_later(3, self._update_participants)
     
-    @event.connect('canvas.mouse_down')
-    def _user_adds_paint(self, *events):
-        for ev in events:
-            relay.new_dot(ev.pos, self.color)
-    
-    @event.prop
-    def color(self, color='#000'):
-        """ Your color.
-        """
-        return str(color)
     
     class JS:
         
@@ -99,23 +101,15 @@ class ColabPainting(ui.Widget):
         def _update_color(self, *events):
             self.canvas.style = 'border: 10px solid ' + events[-1].new_value
         
-        @event.connect('new_dot')
+        @event.connect('paint')
         def _paint_dot(self, *events):
             for ev in events:
-                # Slowly hide old paint
-                self._ctx.globalAlpha = 0.01
-                self._ctx.fillStyle = '#fff'
-                self._ctx.fillRect(0, 0, 400, 400)
-                # Add new dot
                 self._ctx.globalAlpha = 0.8
                 self._ctx.beginPath()
                 self._ctx.fillStyle = ev.color
                 self._ctx.arc(ev.pos[0], ev.pos[1], 5, 0, 6.2831)
                 self._ctx.fill()
 
-
-# Create global relay
-relay = Relay()
 
 if __name__ == '__main__':
     app.serve(ColabPainting)
