de18d841ee6d623b58840aa173cb09e2d28a4535
Almar Klein <almar.klein@gmail.com>
2016-09-22 00:10:26 +0200

    Add TabPanel.current prop (try out Phosphor-Flexx interaction)

--- flexx/ui/layouts/_tabs.py
+++ flexx/ui/layouts/_tabs.py
@@ -3,18 +3,34 @@ Example:
 
 .. UIExample:: 100
     
-    from flexx import app, ui
+    from flexx import app, ui, event
     
     class Example(ui.Widget):
         def init(self):
-            with ui.TabPanel():
+            with ui.TabPanel() as self.t:
                 self.a = ui.Widget(title='red', style='background:#a00;')
                 self.b = ui.Widget(title='green', style='background:#0a0;')
                 self.c = ui.Widget(title='blue', style='background:#00a;')
+                self.d = ui.Widget(title='cyan', style='background:#0aa;')
+    
+        class JS:
+        
+            @event.connect('t.current')
+            def cur_tab_changed(self, *events):
+                prev = events[0].old_value
+                if prev is not None:
+                    prev.title = prev.title.strip(' *')
+                next = events[-1].new_value
+                if next is not None:
+                    next.title = next.title + '*'
+                # Don't like Cyan
+                if next is self.d:
+                    self.t.current = self.a
 """
 
+from ... import event
 from ...pyscript import window
-from . import Layout
+from . import Layout, Widget
 
 
 # class TabBar(Widget):
@@ -105,8 +121,31 @@ class TabPanel(Layout):
         }
     """
     
+    class Both:
+        
+        @event.prop
+        def current(self, v=None):
+            """ The currently shown widget.
+            """
+            if not (v is None or isinstance(v, Widget)):
+                raise ValueError("%s.current should be a Widget, not %r" % (self.id, v))
+            return v
+    
     class JS:
         
         def _init_phosphor_and_node(self):
             self.phosphor = window.phosphor.ui.tabpanel.TabPanel()
             self.node = self.phosphor.node
+            
+            def _phosphor_changes_current(v, info):
+                if info.currentWidget:
+                    self.current = window.flexx.instances[info.currentWidget.id]
+            self.phosphor.currentChanged.connect(_phosphor_changes_current)
+        
+        @event.connect('current')
+        def __current_changed_via_prop(self, *events):
+            w = events[-1].new_value
+            if w is None:
+                self.phosphor.currentWidget = None
+            else:
+                self.phosphor.currentWidget = w.phosphor
