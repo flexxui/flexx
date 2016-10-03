69496e8c5a3805bad9b5961078175d5cabbfea12
Almar Klein <almar.klein@gmail.com>
2016-10-03 11:07:43 +0200

    Add example to test BoxPanel in Widget in VBox

---
+++ flexx/ui/examples/deep2.py
@@ -0,0 +1,34 @@
+"""
+Example to mix BoxPanel and VBox, which at some point failed in Chrome. Now
+Flexx takes precautions to make it work. This example is to test that it
+still works.
+"""
+
+from flexx import app, event, ui
+
+
+class Red(ui.Widget):
+    CSS = '.flx-Red { background: #ff0000;}'
+
+
+class Deep2(ui.Widget):
+    
+    def init(self):
+        
+        with ui.VBox():
+            
+            ui.Label(text='Widgets in BoxPanels in a widget in a vbox')
+        
+            with ui.Widget(flex=1):
+                with ui.BoxPanel(orientation='v'):
+                    with ui.BoxPanel(orientation='h'):
+                        Red(flex=1)
+                        Red(flex=1)
+                    with ui.BoxPanel(orientation='h'):
+                        Red(flex=1)
+                        Red(flex=1)
+
+
+if __name__ == '__main__':
+    m = app.launch(Deep2, 'chromeapp')
+    app.run()
