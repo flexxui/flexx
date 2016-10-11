d05e8a71c881e0d690e5d19fd06008fdc1ed0757
Almar Klein <almar.klein@gmail.com>
2016-10-11 12:06:02 +0200

    fix typo

--- flexx/_config.py
+++ flexx/_config.py
@@ -2,7 +2,7 @@ from .util.config import Config
 
 config = Config('flexx', '~appdata/.flexx.cfg',
     log_level=('info', str, 'The log level to use (DEBUG, INFO, WARNING, ERROR)'),
-    hostname=('localhost', str, 'The default port to serve apps.'),
+    hostname=('localhost', str, 'The default hostname to serve apps.'),
     port=(0, int, 'The default port to serve apps. Zero means auto-select.'),
     webruntime=('', str, 'The default web runtime to use. Default is xul/browser.'),
     ws_timeout=(20, int, 'If the websocket is idle for this time, it is closed.')
