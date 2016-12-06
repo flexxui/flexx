edf07c322946f71502c35a3e22d50ab8c9dc8287
Pierre Gay <pierre.gay@u-bordeaux.fr>
2016-12-06 18:21:59 +0100

    style again

--- flexx/app/examples/serve_ssl.py
+++ flexx/app/examples/serve_ssl.py
@@ -4,10 +4,11 @@ Flexx can be configured to use SSL.
 This example first creates a self-signed certificate and then uses it to create
 a SSL enabled web server (through Tornado ssl_option argument).
 
-Application served through this server is loaded on the browser with 'https' protocol and its websocket is using 'wss'.
+Application served through this server is loaded on the browser with 'https'
+protocol and its websocket is using 'wss'.
 """
 
-from flexx import app, event, ui
+from flexx import app, ui
 
 # generate self-signed certificate for this example
 import os
@@ -21,11 +22,11 @@ os.system('openssl req -x509 -nodes -days 1 -batch -newkey rsa:2048 '
 # Some very secret Model
 class Example(ui.Widget):
     def init(self):
-        ui.Button(text = 'Secret Button')
+        ui.Button(text='Secret Button')
 
 # configure web server for SSL
-app.create_server(ssl_options = {'certfile' : CERTFILE,
-                                 'keyfile' : KEYFILE})
+app.create_server(ssl_options={'certfile' : CERTFILE,
+                               'keyfile' : KEYFILE})
 
 # run application
 app.serve(Example, 'Example')
