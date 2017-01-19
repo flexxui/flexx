3704dc66b338c6e42e0b483ae13215c2b3df1cd1
Almar Klein <almar.klein@gmail.com>
2017-01-19 01:11:30 +0100

    runtime: implement NW.js runtime properly

--- flexx/webruntime/nodewebkit.py
+++ flexx/webruntime/nodewebkit.py
@@ -1,11 +1,10 @@
-""" Web runtime based on node-webkit
+""" Web runtime based on NW.js
 
 https://github.com/nwjs/nw.js
 
 """
 
-# todo: needs more work to discover the nw executable.
-
+import os.path as op
 import os
 import re
 import sys
@@ -14,7 +13,8 @@ import struct
 import shutil
 from urllib.request import urlopen
 
-from .common import DesktopRuntime, create_temp_app_dir
+from .common import DesktopRuntime
+from ._manage import download_runtime, create_temp_app_dir
 
 
 def get_template():
@@ -72,49 +72,83 @@ def fix_libudef(dest):
             os.symlink(path, target)
 
 
-def get_nodewebkit_exe():
-    """ Try to find the executable for node-webkit
-    
-    Return None if it could not be found.
-    """
-    
-    # Get possible locations of nw exe
-    dirs = ['/opt', '~/tools', '~/apps', '~/dev']
-    
-    for dir in dirs:
-        dir = os.path.expanduser(dir)
-        if not os.path.isdir(dir):
-            continue
-        subs = os.listdir(dir)
-        exes = []
-        for sub in subs:
-            if sub.startswith('node-webkit'):
-                exes.append(os.path.join(dir, sub, 'nw'))
-        if exes:
-            exes.sort()
-            return exes[-1]
-    
-    return r'C:\dev\tools\nwjs-v0.19.5-win-x64\nw.exe'
-    return None
-
-
 class NodeWebkitRuntime(DesktopRuntime):
-    """ Desktop runtime for nw.js (http://nwjs.io/, formerly
-    node-webkit), which is based on Chromium and nodejs. Requires nw.js
-    to be installed.
-    """
+    """ Desktop runtime based on NW.js (http://nwjs.io/, formerly
+    node-webkit), which uses the Chromium engine. Flexx takes
+    care of downloading (and updating) the runtime when needed.
     
-    _app_count = 0
+    This runtime is visible in the task manager under a custom process name
+    (``sys,executable + '-ui'``), making it easy to spot in the task manager,
+    and avoids task-bar grouping.
+    """
     
     def _get_name(self):
         return 'nw'
     
+    def _get_latest_version(self):
+        """" Get latest version of the NW runtime.
+        """
+        text = urlopen('https://dl.nwjs.io/').read().decode('utf-8', 'ignore')
+        versions = re.findall('href\=\"v(.+?)\"', text)
+        versions_int = []
+        for v in versions:
+            v = v.strip('/')
+            try:
+                versions_int.append(tuple([int(i) for i in v.split('.')]))
+            except Exception:
+                pass
+        versions_int.sort()
+        return '.'.join([str(i) for i in versions_int[-1]])
+    
+    def _get_url(self, version, platform=None):
+        """ Given a version, get the url where it can be downloaded.
+        """
+        platform = platform or sys.platform
+        if platform.startswith('win'):
+            plat, ext = '-win', '.zip'
+        elif platform.startswith('linux'):
+            plat, ext = '-linux', '.tar.gz'
+        elif platform.startswith('darwin'):
+            plat, ext = '-osx', '.zip'
+        else:
+            raise RuntimeError('Unsupported platform')
+        plat += '-x64' if (struct.calcsize('P') == 8) else '-x64'
+        return 'https://dl.nwjs.io/v' + version + '/nwjs-v' + version + plat + ext
+    
+    def _install_runtime(self):
+        latest_version = self._get_latest_version()
+        url = self._get_url(latest_version)
+        download_runtime(self.get_name(), latest_version, url)
+    
+    def _test_platform(self):
+        if not (sys.platform.startswith('win') or
+                sys.platform.startswith('linux') or
+                sys.platform.startswith('darwin')):
+            raise RuntimeError('NW.js runtime is not supported on platform ' +
+                               sys.platform)
+            # todo: show GUI? that wont work either, but if dialite has a nice fallback, we might as well use that to notify the user
+    
     def _launch(self):
-        NodeWebkitRuntime._app_count += 1
+        
+        self._test_platform()
         
         # Get dir to store app definition
-        app_path = create_temp_app_dir('nw', str(NodeWebkitRuntime._app_count))
+        app_path = create_temp_app_dir('nw')
         id = os.path.basename(app_path).split('_', 1)[1]
+        
+        # Get minimal required version
+        min_version = '0.19.5'
+        # todo: allow configuration
+        
+        # Get runtime exe
+        if False:  # todo: flexx.config.nw_exe
+            exe = flexx.config.nw_exe
+        else:
+            exe = os.path.join(self.get_runtime(min_version), 'nw')
+            exe += '.exe' * sys.platform.startswith('win')
+            # Change exe to avoid grouping + easier recognition in task manager
+            if exe and op.isfile(op.realpath(exe)):
+                exe = self._get_app_exe(exe, app_path)
         
         # Populate app definition
         D = get_template()
@@ -135,54 +169,17 @@ class NodeWebkitRuntime(DesktopRuntime):
         
         # Write
         with open(os.path.join(app_path, 'package.json'), 'wb') as f:
-            f.write(json.dumps(D, indent=4).encode('utf-8'))
+            f.write(json.dumps(D, indent=4).encode())
         
         # Fix libudef bug
-        fix_libudef(app_path)
         llp = os.getenv('LD_LIBRARY_PATH', '')
         if sys.platform.startswith('linux'):
+            fix_libudef(app_path)
             llp = app_path + os.pathsep + llp
         
-        # Launch
-        exe = get_nodewebkit_exe() or 'nw'
-        cmd = [exe, app_path, '--enable-unsafe-es3-apis']  # with webgl2!
-        self._start_subprocess(cmd, LD_LIBRARY_PATH=llp)
-    ##
-    def get_latest_version(self):
-        # return '0.19.5' ? hardcoded by Flexx?
+        # chrome args, enable webgl2, be more compact
+        args = ['--enable-unsafe-es3-apis', '--single-process']
         
-        text = urlopen('https://dl.nwjs.io/').read().decode('utf-8', 'ignore')
-        versions = re.findall('href\=\"v(.+?)\"', text)
-        versions_int = []
-        for v in versions:
-            v = v.strip('/')
-            try:
-                versions_int.append(tuple([int(i) for i in v.split('.')]))
-            except Exception:
-                pass
-        versions_int.sort()
-        return '.'.join([str(i) for i in versions_int[-1]])
-    
-    def get_url(self, version, platform=None):
-        """ Given a version, get the url where it can be downloaded.
-        """
-        platform = platform or sys.platform
-        if platform.startswith('win'):
-            plat = '-win'
-            ext = '.zip'
-        elif platform.startswith('linux'):
-            plat = '-linux'
-            ext = '.tar.gz'
-        elif platform.startswith('darwin'):
-            plat = '-osx'
-            ext = '.zip'
-        else:
-            raise RuntimeError('Unsupported platform')  # todo: detect earlier
-        if struct.calcsize('P') == 8:
-            plat += '-x64'
-        else:
-            plat += '-ia32'
-        return 'https://dl.nwjs.io/v' + version + '/nwjs-v' + version + plat + ext
-    ##
-    
-    
+        # Launch
+        cmd = [exe, app_path] + args
+        self._start_subprocess(cmd, LD_LIBRARY_PATH=llp)
