e7bb9c5b56a5a3087fb5ac765355c4cd3ad78aaf
Almar Klein <almar.klein@gmail.com>
2017-01-19 01:12:04 +0100

    runtime: better implemenation of chrome app runtime

--- flexx/webruntime/chromeapp.py
+++ flexx/webruntime/chromeapp.py
@@ -16,9 +16,11 @@ import os
 import sys
 
 from .common import DesktopRuntime
+from ._manage import RUNTIME_DIR
 
-# todo: icon, sizing, etc.
-# --window-size=200,200
+
+# todo: icon,  etc.
+
 
 def get_chrome_exe():
     """ Get the path of the Chrome executable
@@ -82,17 +84,42 @@ def get_chromium_exe():
 
 
 class ChromeAppRuntime(DesktopRuntime):
-    """ Desktop runtime based on chrome app. Requires the Chrome or
-    Chromium browser to be installed.
+    """ Desktop runtime based on the Chrome/Chromium browser. This runtime
+    is somewhat limited in that it has a Chrome icon on Linux and the app
+    tends to group on the taskbar with the Chrome/Chromium browser. 
     
     Note: icon, sizing and title is not yet supported.
     """
     
+    def _get_name(self):
+        return 'chrome'
+    
+    def _install_runtime(self):
+        version = 'latest'
+        path = os.path.join(RUNTIME_DIR, self.get_name() + '_' + version)
+        if not os.path.isdir(path):
+            os.mkdir(path)
+        with open(os.path.join(path, 'stub.txt'), 'wb') as f:
+            f.write('Flexx uses the system Chrome'.encode())
+    
     def _launch(self):
         # Get chrome executable
+        self.get_runtime('latest')
         exe = get_chrome_exe() or get_chromium_exe()
         if exe is None:
             raise RuntimeError('Chrome or Chromium browser was not detected.')
+            # todo: dialite
+        
+        # Options
+        size = self._kwargs.get('size', (640, 480))
+        pos = self._kwargs.get('pos', None)
+        #
+        opts = ['--incognito']
+        opts.append('--enable-unsafe-es3-apis')  # enable webgl2
+        opts.append('--window-size=%i,%i' %  (size[0], size[1]))
+        if pos:
+            opts.append('--window-position=%i,%i' %  (pos[0], pos[1]))
+        
         # Launch url
         url = self._kwargs['url']
-        self._start_subprocess([exe, '--incognito', '--app=%s' % url])
+        self._start_subprocess([exe, '--app=%s' % url] + opts)
