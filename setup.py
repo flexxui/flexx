20e7c80bdd45c19ca9624b18cadbab542fe7ee83
Almar Klein <almar.klein@gmail.com>
2017-02-21 15:24:49 +0100

    Include Python 3.6 in test matrix

--- setup.py
+++ setup.py
@@ -151,5 +151,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
