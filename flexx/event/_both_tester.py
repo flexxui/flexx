85bed1762c61c77c271796d5cb1ef198085df8db
Almar Klein <almar.klein@gmail.com>
2017-10-24 13:54:48 +0200

    event: improve bothtester utility

---
+++ flexx/event/_both_tester.py
@@ -0,0 +1,180 @@
+"""
+This module defines a utility to write tests for flexx.event
+that run both in Python and JS.
+
+This is part of flexx.event and not in flexx/event/tests because (re)import
+during tests.
+"""
+
+import sys
+
+from flexx import event
+from flexx.event import loop
+from flexx.event._js import create_js_component_class, JS_EVENT
+from flexx.pyscript.functions import py2js, evaljs 
+from flexx.pyscript.stdlib import get_std_info, get_partial_std_lib
+
+
+def this_is_js():
+    return False
+
+
+class StdoutMismatchError(Exception):
+    """ Raised when the stdout mismatches.
+    """
+    pass
+
+
+class FakeStream:
+    """ To capture Pythons stdout and stderr during the both-tests.
+    """
+    
+    def __init__(self):
+        self._parts = []
+    
+    def write(self, msg):
+        # Keep single messages together, so that errors are compared as one "line"
+        msg2 = msg.replace('\n', '\r')
+        if msg.endswith('\n'):
+            self._parts.append(msg2[:-1] + '\n')
+        else:
+            self._parts.append(msg2)
+    
+    def flush(self):
+        pass
+    
+    def getvalue(self):
+        return ''.join(self._parts)
+
+
+def call_func_in_py(func):
+    """ Call a function and capture ints stdout.
+    """
+    loop.reset()
+    orig_stdout = sys.stdout
+    orig_stderr = sys.stderr
+    fake_stdout = FakeStream()
+    sys.stdout = sys.stderr = fake_stdout
+    try:
+        func()
+    except Exception as err:
+        raise  # fall through
+    finally:
+        sys.stdout = orig_stdout
+        sys.stderr = orig_stderr
+    return fake_stdout.getvalue().rstrip()
+
+
+def call_func_in_js(func, classes):
+    # Collect base classes
+    all_classes = []
+    for cls in classes:
+        for c in cls.mro():
+            if c is event.Component or c in all_classes:
+                break
+            all_classes.append(c)
+    # Generate JS code
+    code = JS_EVENT
+    for c in reversed(all_classes):
+        code += create_js_component_class(c, c.__name__,
+                                          c.__bases__[0].__name__+'.prototype')
+    code += py2js(func, 'test', inline_stdlib=False, docstrings=False)
+    code += 'test();'
+    nargs, function_deps, method_deps = get_std_info(code)
+    code = get_partial_std_lib(function_deps, method_deps, []) + code
+    # Call
+    return evaljs(code, print_result=False)  # allow using file
+
+
+def smart_compare(kinds, text1, text2, what=''):
+    """ Compare two texts, first being the reference,
+    raising an error that shows where the texts differ.
+    """
+    lines1 = text1.split('\n')
+    lines2 = text2.split('\n')
+    n = max(len(lines1), len(lines2))
+    
+    while len(lines1) < n:
+        lines1.append('<empty>')
+    while len(lines2) < n:
+        lines2.append('<empty>')
+    
+    refpfx = '\xaf   '
+    
+    for i in range(n):
+        line1, line2 = lines1[i], lines2[i]
+        line1 = line1.lower()
+        line2 = line2.lower()
+        if line1.startswith('?'):
+            equal_enough = line1[1:].strip() in line2
+        else:
+            equal_enough = line1 == line2
+        if not equal_enough:
+            i1 = max(0, i - 3)
+            i2 = min(n, i + 3)
+            msg = ''
+            for j in range(i1, i2):
+                linenr = str(j + 1).rjust(2, '0')
+                prefix = ' >> ' if j == i else '    '
+                msg += '{}{} {} {}'.format(refpfx, kinds[0], linenr,
+                                           lines1[j].replace(' ', '\xb7') + '\n')
+                msg += '{}{} {} {}'.format(prefix, kinds[1], linenr,
+                                           lines2[j].replace(' ', '\xb7') + '\n')
+            raise StdoutMismatchError('Text mismatch in %s:\n%s ' % (what, msg))
+
+# todo: do I use the split option? If not, compare ref py and js in one go
+
+def run_in_both(*classes, js=True, py=True):
+    """ Decorator to run a test in both Python and JS.
+    
+    The decorator should be provided with any Component classes that
+    you want to use in the test.
+    
+    The function docstring should match the stdout + stderr of the test (case
+    insensitive). To provide separate reference outputs for Python and
+    JavaScript, use a delimiter of at least 10 '-' characters. Use "? xx"
+    to test that "xx" is present on a line (useful for logged exceptions).
+    """
+    
+    def wrapper(func):
+        reference = '\n'.join(line[4:] for line in func.__doc__.splitlines())
+        parts = reference.split('-'*10)
+        pyref = parts[0].strip(' \n')
+        jsref = parts[-1].strip(' \n-')
+        
+        def runner1():
+            # One level of indirection to make cleaner error reporting by pytest
+            err = None
+            try:
+                return runner2()
+            except Exception as e:
+                err = e
+            if isinstance(err, StdoutMismatchError):
+                raise StdoutMismatchError(err)
+            elif isinstance(err, RuntimeError):
+                raise RuntimeError(err)
+            else:
+                raise err
+        
+        def runner2():
+            # Run in Python
+            if py:
+                pyresult = call_func_in_py(func)
+                pyresult = pyresult.replace('"', "'").replace("\\'", "'")
+                pyresult = pyresult.split('!!!!')[-1]
+                #print('Py:\n' + pyresult)
+            # Run in JS
+            if js:
+                jsresult = call_func_in_js(func, classes)
+                jsresult = jsresult.replace('[ ', '[').replace(' ]', ']')
+                jsresult = jsresult.replace('\n  ', ' ')
+                jsresult = jsresult.replace('"', "'").split('!!!!')[-1]
+                #print('JS:\n' + jsresult)
+            if py:
+                smart_compare('rp', pyref, pyresult, func.__name__ + '() in Python')
+            if js:
+                smart_compare('rj', jsref, jsresult, func.__name__ + '() in JavaScript')
+            print(func.__name__, 'ok')
+            return True
+        return runner1
+    return wrapper
---
+++ flexx/event/tests/test_bothtester.py
@@ -0,0 +1,164 @@
+"""
+This is a test of tests. Test that the bothtester functionality does
+what its supposed to do. Most imortantly, it should fail at the right
+moments.
+"""
+
+from flexx.util.testing import run_tests_if_main, skipif, skip, raises
+from flexx.event._both_tester import run_in_both, StdoutMismatchError
+
+from flexx import event
+
+loop = event.loop
+logger = event.logger
+
+
+def this_is_js():
+    return False
+
+
+class Person(event.Component):
+   
+    first_name = event.StringProp('John', settable=True)
+    last_name = event.StringProp('Doe', settable=True)
+
+
+##
+
+@run_in_both(Person)
+def func_ok1():
+    """
+    john doe
+    john doe
+    almar klein
+    """
+    p = Person()
+    print(p.first_name, p.last_name)
+    p.set_first_name('almar')
+    p.set_last_name('klein')
+    print(p.first_name, p.last_name)
+    loop.iter()
+    print(p.first_name, p.last_name)
+
+
+def test_ok1():
+    assert func_ok1()
+    
+##
+
+@run_in_both()
+def func_ok2():
+    """
+    bar
+    ----------
+    foo
+    """
+    if this_is_js():
+        print('foo')
+    else:
+        print('bar')
+
+@run_in_both()
+def func_ok3():
+    """
+    bar
+    """
+    if this_is_js():
+        print('foo')
+    else:
+        print('bar')
+
+@run_in_both()
+def func_ok4():
+    """
+    foo
+    """
+    if this_is_js():
+        print('foo')
+    else:
+        print('bar')
+
+
+def test_ok234():
+    assert func_ok2()
+    with raises(StdoutMismatchError):
+        func_ok3()
+    with raises(StdoutMismatchError):
+        func_ok4()
+
+
+##
+
+@run_in_both(Person)
+def func_fail():
+    """
+    john doe
+    almar klein
+    """
+    p = Person()
+    print(p.first_name, p.last_name)
+    p.set_first_name('almar')
+    p.set_last_name('klein')
+    print(p.first_name, p.last_name)
+    loop.iter()
+    print(p.first_name, p.last_name)
+
+
+def test_fail():
+    with raises(StdoutMismatchError):
+        func_fail()
+
+##
+
+@run_in_both()
+def func_ok_exception():  # ? indicates that the following text must be present
+    """
+    ? AttributeError
+    """
+    try:
+        raise AttributeError('xx')
+    except Exception as err:
+        logger.exception(err)
+
+
+def test_ok_exception():
+    assert func_ok_exception()
+
+
+##
+
+@run_in_both()
+def func_fail_exception1():  # This just fails hard
+    """
+    """
+    raise AttributeError('xx')
+
+@run_in_both(js=False)
+def func_fail_exception2():  # This just fails hard
+    """
+    """
+    raise AttributeError('xx')
+
+@run_in_both(py=False)
+def func_fail_exception3():  # This just fails hard
+    """
+    """
+    raise AttributeError('xx')
+
+
+def test_fail_exception():
+    with raises(AttributeError):
+        func_fail_exception1()
+    with raises(AttributeError):
+        func_fail_exception2()
+    with raises(Exception):  # eval_js turns processerror into Exception
+        func_fail_exception3()
+
+
+if __name__ == '__main__':
+    # Run this as a script to 
+    test_ok1()
+    test_ok234()
+    test_ok_exception()
+    test_fail()
+    test_fail_exception()
