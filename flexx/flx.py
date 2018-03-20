84c4ce576b142f094e5d682759713b602ebc9698
Almar Klein <almar.klein@gmail.com>
2018-03-20 14:46:55 +0100

    New flx namespace

---
+++ flexx/flx.py
@@ -0,0 +1,10 @@
+"""
+The flexx.flx module provides a namspace combining all the things from
+flexx.app, flexx.event, and flexx.ui.
+"""
+
+# flake8: noqa
+
+from .event import *
+from .app import *
+from .ui import *
