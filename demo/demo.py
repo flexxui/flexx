2def36a428d3559ec6203727298254a7c09141c3
Almar Klein <almar.klein@gmail.com>
2019-10-26 23:41:32 +0200

    tweaks to demo container code

--- demo/demo.py
+++ demo/demo.py
@@ -2,6 +2,9 @@
 Demo server.
 """
 
+import sys
+import asyncio
+
 from flexx import app
 
 from flexxamples.demos.monitor import Monitor
@@ -12,10 +15,20 @@ from flexxamples.demos.d3_collision import CollisionDemo
 from flexxamples.demos.plotly_gdp import PlotlyGeoDemo
 
 
+async def exit_server_after_a_while():
+    # Exit the server after 12 hours, after which it will start up again
+    # (using Docker with auto-restart). This makes sure that the server
+    # is not bother too much in case we have a memory leak.
+    await asyncio.sleep(12 * 3600)
+    sys.exit()
+
+asyncio.ensure_future(exit_server_after_a_while())
+
+
 if __name__ == '__main__':
     # This example is setup as a server app
-    app.serve(Monitor)
-    app.serve(ChatRoom)
+    # app.serve(Monitor)
+    # app.serve(ChatRoom)
     app.serve(ColabPainting)
     app.serve(CollisionDemo)
     # app.serve(PlotlyGeoDemo)  # CORS fail?
--- demo/deploy.py
+++ demo/deploy.py
@@ -10,13 +10,13 @@ import zipfile
 import requests
 
 
-DEPLOYER_ACCESS_TOKEN = os.getenv('DEPLOYER_ACCESS_TOKEN_SITE')
+DEPLOYER_ACCESS_TOKEN = os.getenv("DEPLOYER_ACCESS_TOKEN_SITE")
 SITE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def deploy():
     imagename = 'flexxdemo:1'
-    
+
     # Zip it up
     f = io.BytesIO()
     with zipfile.ZipFile(f, 'w') as zf:
@@ -25,9 +25,9 @@ def deploy():
             if os.path.isfile(fullname):
                 bb = open(fullname, 'rb').read()
                 zf.writestr(os.path.relpath(fullname, SITE_DIR), bb)
-    
+
     # POST
-    url = 'https://deploy.canpute.com/{}/{}'.format(imagename, DEPLOYER_ACCESS_TOKEN)
+    url = "https://deploy.canpute.com/{}?token={}".format(imagename, DEPLOYER_ACCESS_TOKEN)
     r = requests.post(url, data=f.getvalue())
     if r.status_code != 200:
         raise RuntimeError('Publish failed: ' + r.text)
